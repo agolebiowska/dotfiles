5e4648a0997cfecd00bdf9fef8cd3783c2b86955
agolebiowska <agataxgolebiowska@gmail.com>
2023-01-30 01:02:56 +0000

    .

---
+++ .config/dmenu/themes/catppuccin.h
@@ -0,0 +1,7 @@
+static const char *colors[SchemeLast][2] = {
+	[SchemeNorm] = { "#c6d0f5", "#303446" },
+	[SchemeSel] = { "#303446", "#99d1db" },
+	[SchemeOut] = { "#000000", "#99d1db" },
+	[SchemeSelHighlight] = { "#f4b8e4", "#949cbb" },
+	[SchemeNormHighlight] = { "#f4b8e4", "#737994" },
+};
