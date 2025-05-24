c15c5102df97d4d61f0b64af0def952278511cf3
Agata Gołębiowska <gagata@google.com>
2025-05-24 14:55:40 +0000

    separate for mac and deb

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
-ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
 ln -sf $(pwd)/zsh/.p10k.zsh ~/.p10k.zsh
 ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 
-ln -sf $(pwd)/ghostty ~/.config/ghostty
-ln -sf $(pwd)/kitty ~/.config/kitty
 ln -sf $(pwd)/fastfetch ~/.config/fastfetch
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "# macOS specific settings" >> ~/.zshrc.local
+    echo "I'm on MacOS!"
+    ln -sf $(pwd)/zsh/.zshrc.mac ~/.zshrc
+    ln -sf $(pwd)/ghostty ~/.config/ghostty
+    ln -sf $(pwd)/kitty ~/.config/kitty
     ln -sf $(pwd)/yabai ~/.config/yabai
     ln -sf $(pwd)/skhd ~/.config/skhd
     ln -sf $(pwd)/sketchybar ~/.config/sketchybar
     ln -sf $(pwd)/karabiner  ~/.config/karabiner
+
 	
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "# Linux specific settings" >> ~/.zshrc.local
+    echo "I'm on Debian!"
+    ln -sf $(pwd)/zsh/.zshrc.deb ~/.zshrc
 fi
 
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
